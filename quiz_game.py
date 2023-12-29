527348e534e75dffa5e3679cce185f77a922fd1d
Taymaz Naghashi <mrtaimaznaghashi@gmail.com>
2023-12-29 16:41:26 -0500

    start

---
+++ quiz_game.py
@@ -0,0 +1,22 @@
+#-----------------------------------------------------
+points= 0 
+qu="What is the first color in the rainbow"
+a=("a")
+b=("b")
+c=("c")
+d=("d")
+#-----------------------------------------------------
+def question():
+    print("----------------------------------")
+    print ("PYTHON TRIVIA GAME!")
+    print("Points: "+ str(points))
+    print("QUESTION:"+qu)
+    print("option A:"+a)
+    print("option B:"+b)
+    print("option C:"+c)
+    print("option D:"+d)
+    print("----------------------------------")
+    answere=input("Witch One Is Correct?")
+    print("----------------------------------")
+question()
+#-----------------------------------------------------
